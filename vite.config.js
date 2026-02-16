2a4a61bee61ee30d2f05bbf01ff4d63f4b68afe5
Ranvil Mae Reyes <85659337+RanvilMae@users.noreply.github.com>
2026-02-16 16:41:28 +0800

    Updated tailwind css and vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        tailwindcss(), // This is the v4 engine
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
