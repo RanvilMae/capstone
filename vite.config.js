ae33fc0e6f5a6f6362af00c168c285c4413cff82
Ranvil Mae Reyes <85659337+RanvilMae@users.noreply.github.com>
2026-02-18 09:24:52 +0800

    Updated npm uninstall postcss autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'; // The v4 plugin
 
 export default defineConfig({
     plugins: [
-        tailwindcss(), // This must come before laravel()
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
